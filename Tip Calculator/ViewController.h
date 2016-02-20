44459f2358befcdd1d93012934cf0cc6935fd094
Sukrith Akula <Sukrith@sukriths-macbook-pro.local>
2016-02-19 21:05:46 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Sukrith Akula on 2/19/16.
+//  Copyright © 2016 Sukrith Akula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
